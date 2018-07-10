92f2103df88e53a91a1af40803eb77942c1cc7c0
Karoly Erdos <karoly@ebi.ac.uk>
2018-07-10 16:50:58 +0100

    Fix tests

--- src/main/java/uk/ac/ebi/subs/filecontentvalidator/service/FileType.java
+++ src/main/java/uk/ac/ebi/subs/filecontentvalidator/service/FileType.java
@@ -22,6 +22,6 @@ public enum FileType {
     }
 
     public static boolean forName(String name) {
-        return SUPPORTED_TYPES.contains(name);
+        return SUPPORTED_TYPES.contains(name.toUpperCase());
     }
 }
